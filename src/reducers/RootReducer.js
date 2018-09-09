f4908d8cb51095a195c120eb329243476f4459e5
Andry Panasiyk <andrypanasiyk@gmail.com>
2018-09-10 00:20:38 +0300

    Updates

--- src/reducers/RootReducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import ItemsReducer from './ItemsReducer'
-import CommentsReducer from './CommentsReducer'
-
-const RootReducer = combineReducers({
-    ItemsReducer,
-    CommentsReducer
-});
-export default RootReducer;
